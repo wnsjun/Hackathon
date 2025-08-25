12822fc9ecdcf4eb878571f5bd7c64e1a42e8a8a
seungju <lsjxcs@gmail.com>
2025-08-25 20:53:05 +0900

    주석삭제

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,36 +1,3 @@
-// const Button = ({
-//   children,
-//   onClick,
-//   disabled = false,
-//   color = 'green',
-//   type = 'button',
-//   ...props
-// }) => {
-//   const baseClasses =
-//     'px-4 py-2 rounded text-white font-semibold focus:outline-none transition';
-//   const colors = {
-//     blue: 'bg-blue-600 hover:bg-blue-700 disabled:bg-blue-300',
-//     red: 'bg-red-600 hover:bg-red-700 disabled:bg-red-300',
-//     green: 'bg-green-600 hover:bg-green-700 disabled:bg-green-300',
-//     gray: 'bg-gray-600 hover:bg-gray-700 disabled:bg-gray-300',
-//   };
-//   const colorClasses = colors[color] || colors.green;
-
-//   return (
-//     <button
-//       type={type}
-//       className={`${baseClasses} ${colorClasses} ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
-//       onClick={disabled ? undefined : onClick}
-//       disabled={disabled}
-//       {...props}
-//     >
-//       {children}
-//     </button>
-//   );
-// };
-
-// export default Button;
-
 const Button = ({
   children,
   onClick,
